f2ba8f12d537321a3bf5e7da2709f83241d03aec
Arturo Romero <aromero@aeonlinesolutions.com>
2022-02-28 17:55:48 -0500

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src", "assets/images"),
+          from: path.resolve(__dirname, "src", "assets/images/"),
           to: "assets/images"
         }
       ]
